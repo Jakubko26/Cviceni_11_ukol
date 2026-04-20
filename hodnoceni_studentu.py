98ba9d605a93ab8596660d3fdfad3a71170eee4e
Jakub Michalík <jmmichali2@gmail.com>
2026-04-20 14:20:41 +0200

    COmmit4

--- hodnoceni_studentu.py
+++ hodnoceni_studentu.py
@@ -1,6 +1,35 @@
+import matplotlib.pyplot as plt
 class StudentsGrades:
+    def plot_histogram(self):
+        plt.hist(self.scores)
+        plt.xlabel("body")
+        plt.ylabel("pocet studentu")
+        plt.title("rozdelenie bodov studentom")
+        plt.show()
     def __init__(self, scores):
         self.scores = scores
+        self._sorted_scores = None
+    def find_sorted(self,score):
+        if self._sorted_scores is None:
+            print("sorting...")
+            self._sorted_scores = self.get_sorted()
+
+        arr = self._sorted_scores
+
+        left = 0
+        right = len(arr) -1
+
+        while left <= right:
+            mid = (left + right) //2
+
+            if arr[mid] == score:
+                return mid
+            elif arr[mid] < score:
+                left = mid +1
+            else:
+                right = mid -1
+        return None
+
 
     def get_by_index(self, index):
         return self.scores[index]
@@ -52,5 +81,10 @@ def main():
     random_results = StudentsGrades(random_numbers(30, 0, 100))
     print(random_results.count())
     print(random_results.get_sorted())
+    results.plot_histogram()
+    random_results.plot_histogram()
+    print(results.find_sorted(91))  # sorting…  → index 7
+    print(results.find_sorted(50))  # → index 2 (už neřadí)
+    print(results.find_sorted(77))  # → None  (77 tam není)
 if __name__ == "__main__":
     main()
